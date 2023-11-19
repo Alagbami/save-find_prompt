c7251912dbeb11bf681d4dadcbc834c2b9ce5a2e
Adeleke <adelekeabiodun1996@gmail.com>
2023-11-19 20:59:24 +0100

    Trigger revalidation to fetch the updated prompts

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+/*"use client";
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
@@ -9,7 +9,7 @@ import Form from "@components/Form";
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
-
+  
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
 
@@ -40,6 +40,58 @@ const CreatePrompt = () => {
   return (
     <Form
       type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;*/
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+        // Trigger revalidation to fetch the updated prompts
+        router.replace("/create-prompt");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
       post={post}
       setPost={setPost}
       submitting={submitting}
