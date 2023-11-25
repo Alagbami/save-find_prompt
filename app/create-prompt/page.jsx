3da671160e95b1e980e494bc5226b7d0fee2bdcf
Adeleke <adelekeabiodun1996@gmail.com>
2023-11-25 12:07:37 +0100

    add useEffect and initialDATA Form props

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,67 +1,24 @@
-/*"use client";
-
-import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
-import Form from "@components/Form";
-
-const CreatePrompt = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  
-  const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-
-  const createPrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
-  return (
-    <Form
-      type='Create'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-    />
-  );
-};
-
-export default CreatePrompt;*/
 "use client";
-import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import Form from "@components/Form";
 
-const CreatePrompt = () => {
-  const router = useRouter();
+const CreatePrompt = (props) => {
   const { data: session } = useSession();
-
+  const [initialData, setInitialData] = useState(props.initialData || {});
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const fetchInitialData = async () => {
+    try {
+      const response = await fetch("/api/prompt/new");
+      const data = await response.json();
+      setInitialData(data);
+    } catch (error) {
+      console.error("Error fetching initial data:", error);
+    }
+  };
 
   const createPrompt = async (e) => {
     e.preventDefault();
@@ -78,16 +35,20 @@ const CreatePrompt = () => {
       });
 
       if (response.ok) {
-        router.push("/");
-        // Trigger revalidation to fetch the updated prompts
-        router.replace("/");
+        // Use window.location to navigate
+        window.location.href = "/";
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     } finally {
       setIsSubmitting(false);
     }
   };
+
+  // Fetch initial data when the component mounts
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
 
   return (
     <Form
@@ -96,8 +57,10 @@ const CreatePrompt = () => {
       setPost={setPost}
       submitting={submitting}
       handleSubmit={createPrompt}
+      initialData={initialData}
     />
   );
 };
 
 export default CreatePrompt;
+
